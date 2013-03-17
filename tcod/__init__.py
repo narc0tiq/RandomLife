8aa18e5dbfcdc6ed1f6f89bda0ddef8ed77ae10b
Octav Sandulescu <github@narc.ro>
2013-03-17 04:04:41 +0200

    Add the color constants and remove const dependency

--- tcod/__init__.py
+++ tcod/__init__.py
@@ -1,5 +1,6 @@
 import libtcodpy as libtcod
-from game import const
+
+Color = libtcod.Color
 
 class Random:
     def __init__(self, stream_id):
@@ -114,7 +115,7 @@ class Console:
     def put_char(self, x, y, char, flags=libtcod.BKGND_NONE):
         return libtcod.console_put_char(self.console_id, x, y, char, flags)
 
-    def print_ex(self, x, y, flags, align, text):
+    def print_ex(self, x, y, flags=libtcod.BKGND_NONE, align=libtcod.LEFT, text="DEFAULT_TEXT"):
         return libtcod.console_print_ex(self.console_id, x, y, flags, align, text)
 
     def rect(self, x, y, width, height, clear=False, effect=libtcod.BKGND_SET):
@@ -135,8 +136,7 @@ class Map:
     def set_properties(self, x, y, see_through, pass_through):
         return libtcod.map_set_properties(self.map, x, y, see_through, pass_through)
 
-    def compute_fov(self, x, y, radius=const.FOV_RADIUS,
-                    light_walls=const.FOV_LIGHT_WALLS, algorithm=const.FOV_ALGORITHM):
+    def compute_fov(self, x, y, radius, light_walls, algorithm):
         return libtcod.map_compute_fov(self.map, x, y, radius, light_walls, algorithm)
 
     def is_in_fov(self, x, y):
@@ -161,3 +161,212 @@ BACKGROUND_DEFAULT = libtcod.BKGND_DEFAULT
 ALIGN_LEFT = libtcod.LEFT
 ALIGN_RIGHT = libtcod.RIGHT
 ALIGN_CENTER = libtcod.CENTER
+
+COLOR_BLACK = libtcod.black
+COLOR_DARKEST_GREY = libtcod.darkest_grey
+COLOR_DARKER_GREY = libtcod.darker_grey
+COLOR_DARK_GREY = libtcod.dark_grey
+COLOR_GREY = libtcod.grey
+COLOR_LIGHT_GREY = libtcod.light_grey
+COLOR_LIGHTER_GREY = libtcod.lighter_grey
+COLOR_LIGHTEST_GREY = libtcod.lightest_grey
+COLOR_DARKEST_GRAY = libtcod.darkest_gray
+COLOR_DARKER_GRAY = libtcod.darker_gray
+COLOR_DARK_GRAY = libtcod.dark_gray
+COLOR_GRAY = libtcod.gray
+COLOR_LIGHT_GRAY = libtcod.light_gray
+COLOR_LIGHTER_GRAY = libtcod.lighter_gray
+COLOR_LIGHTEST_GRAY = libtcod.lightest_gray
+COLOR_WHITE = libtcod.white
+
+COLOR_DARKEST_SEPIA = libtcod.darkest_sepia
+COLOR_DARKER_SEPIA = libtcod.darker_sepia
+COLOR_DARK_SEPIA = libtcod.dark_sepia
+COLOR_SEPIA = libtcod.sepia
+COLOR_LIGHT_SEPIA = libtcod.light_sepia
+COLOR_LIGHTER_SEPIA = libtcod.lighter_sepia
+COLOR_LIGHTEST_SEPIA = libtcod.lightest_sepia
+
+COLOR_RED = libtcod.red
+COLOR_FLAME = libtcod.flame
+COLOR_ORANGE = libtcod.orange
+COLOR_AMBER = libtcod.amber
+COLOR_YELLOW = libtcod.yellow
+COLOR_LIME = libtcod.lime
+COLOR_CHARTREUSE = libtcod.chartreuse
+COLOR_GREEN = libtcod.green
+COLOR_SEA = libtcod.sea
+COLOR_TURQUOISE = libtcod.turquoise
+COLOR_CYAN = libtcod.cyan
+COLOR_SKY = libtcod.sky
+COLOR_AZURE = libtcod.azure
+COLOR_BLUE = libtcod.blue
+COLOR_HAN = libtcod.han
+COLOR_VIOLET = libtcod.violet
+COLOR_PURPLE = libtcod.purple
+COLOR_FUCHSIA = libtcod.fuchsia
+COLOR_MAGENTA = libtcod.magenta
+COLOR_PINK = libtcod.pink
+COLOR_CRIMSON = libtcod.crimson
+
+COLOR_DARK_RED = libtcod.dark_red
+COLOR_DARK_FLAME = libtcod.dark_flame
+COLOR_DARK_ORANGE = libtcod.dark_orange
+COLOR_DARK_AMBER = libtcod.dark_amber
+COLOR_DARK_YELLOW = libtcod.dark_yellow
+COLOR_DARK_LIME = libtcod.dark_lime
+COLOR_DARK_CHARTREUSE = libtcod.dark_chartreuse
+COLOR_DARK_GREEN = libtcod.dark_green
+COLOR_DARK_SEA = libtcod.dark_sea
+COLOR_DARK_TURQUOISE = libtcod.dark_turquoise
+COLOR_DARK_CYAN = libtcod.dark_cyan
+COLOR_DARK_SKY = libtcod.dark_sky
+COLOR_DARK_AZURE = libtcod.dark_azure
+COLOR_DARK_BLUE = libtcod.dark_blue
+COLOR_DARK_HAN = libtcod.dark_han
+COLOR_DARK_VIOLET = libtcod.dark_violet
+COLOR_DARK_PURPLE = libtcod.dark_purple
+COLOR_DARK_FUCHSIA = libtcod.dark_fuchsia
+COLOR_DARK_MAGENTA = libtcod.dark_magenta
+COLOR_DARK_PINK = libtcod.dark_pink
+COLOR_DARK_CRIMSON = libtcod.dark_crimson
+
+COLOR_DARKER_RED = libtcod.darker_red
+COLOR_DARKER_FLAME = libtcod.darker_flame
+COLOR_DARKER_ORANGE = libtcod.darker_orange
+COLOR_DARKER_AMBER = libtcod.darker_amber
+COLOR_DARKER_YELLOW = libtcod.darker_yellow
+COLOR_DARKER_LIME = libtcod.darker_lime
+COLOR_DARKER_CHARTREUSE = libtcod.darker_chartreuse
+COLOR_DARKER_GREEN = libtcod.darker_green
+COLOR_DARKER_SEA = libtcod.darker_sea
+COLOR_DARKER_TURQUOISE = libtcod.darker_turquoise
+COLOR_DARKER_CYAN = libtcod.darker_cyan
+COLOR_DARKER_SKY = libtcod.darker_sky
+COLOR_DARKER_AZURE = libtcod.darker_azure
+COLOR_DARKER_BLUE = libtcod.darker_blue
+COLOR_DARKER_HAN = libtcod.darker_han
+COLOR_DARKER_VIOLET = libtcod.darker_violet
+COLOR_DARKER_PURPLE = libtcod.darker_purple
+COLOR_DARKER_FUCHSIA = libtcod.darker_fuchsia
+COLOR_DARKER_MAGENTA = libtcod.darker_magenta
+COLOR_DARKER_PINK = libtcod.darker_pink
+COLOR_DARKER_CRIMSON = libtcod.darker_crimson
+
+COLOR_DARKEST_RED = libtcod.darkest_red
+COLOR_DARKEST_FLAME = libtcod.darkest_flame
+COLOR_DARKEST_ORANGE = libtcod.darkest_orange
+COLOR_DARKEST_AMBER = libtcod.darkest_amber
+COLOR_DARKEST_YELLOW = libtcod.darkest_yellow
+COLOR_DARKEST_LIME = libtcod.darkest_lime
+COLOR_DARKEST_CHARTREUSE = libtcod.darkest_chartreuse
+COLOR_DARKEST_GREEN = libtcod.darkest_green
+COLOR_DARKEST_SEA = libtcod.darkest_sea
+COLOR_DARKEST_TURQUOISE = libtcod.darkest_turquoise
+COLOR_DARKEST_CYAN = libtcod.darkest_cyan
+COLOR_DARKEST_SKY = libtcod.darkest_sky
+COLOR_DARKEST_AZURE = libtcod.darkest_azure
+COLOR_DARKEST_BLUE = libtcod.darkest_blue
+COLOR_DARKEST_HAN = libtcod.darkest_han
+COLOR_DARKEST_VIOLET = libtcod.darkest_violet
+COLOR_DARKEST_PURPLE = libtcod.darkest_purple
+COLOR_DARKEST_FUCHSIA = libtcod.darkest_fuchsia
+COLOR_DARKEST_MAGENTA = libtcod.darkest_magenta
+COLOR_DARKEST_PINK = libtcod.darkest_pink
+COLOR_DARKEST_CRIMSON = libtcod.darkest_crimson
+
+COLOR_LIGHT_RED = libtcod.light_red
+COLOR_LIGHT_FLAME = libtcod.light_flame
+COLOR_LIGHT_ORANGE = libtcod.light_orange
+COLOR_LIGHT_AMBER = libtcod.light_amber
+COLOR_LIGHT_YELLOW = libtcod.light_yellow
+COLOR_LIGHT_LIME = libtcod.light_lime
+COLOR_LIGHT_CHARTREUSE = libtcod.light_chartreuse
+COLOR_LIGHT_GREEN = libtcod.light_green
+COLOR_LIGHT_SEA = libtcod.light_sea
+COLOR_LIGHT_TURQUOISE = libtcod.light_turquoise
+COLOR_LIGHT_CYAN = libtcod.light_cyan
+COLOR_LIGHT_SKY = libtcod.light_sky
+COLOR_LIGHT_AZURE = libtcod.light_azure
+COLOR_LIGHT_BLUE = libtcod.light_blue
+COLOR_LIGHT_HAN = libtcod.light_han
+COLOR_LIGHT_VIOLET = libtcod.light_violet
+COLOR_LIGHT_PURPLE = libtcod.light_purple
+COLOR_LIGHT_FUCHSIA = libtcod.light_fuchsia
+COLOR_LIGHT_MAGENTA = libtcod.light_magenta
+COLOR_LIGHT_PINK = libtcod.light_pink
+COLOR_LIGHT_CRIMSON = libtcod.light_crimson
+
+COLOR_LIGHTER_RED = libtcod.lighter_red
+COLOR_LIGHTER_FLAME = libtcod.lighter_flame
+COLOR_LIGHTER_ORANGE = libtcod.lighter_orange
+COLOR_LIGHTER_AMBER = libtcod.lighter_amber
+COLOR_LIGHTER_YELLOW = libtcod.lighter_yellow
+COLOR_LIGHTER_LIME = libtcod.lighter_lime
+COLOR_LIGHTER_CHARTREUSE = libtcod.lighter_chartreuse
+COLOR_LIGHTER_GREEN = libtcod.lighter_green
+COLOR_LIGHTER_SEA = libtcod.lighter_sea
+COLOR_LIGHTER_TURQUOISE = libtcod.lighter_turquoise
+COLOR_LIGHTER_CYAN = libtcod.lighter_cyan
+COLOR_LIGHTER_SKY = libtcod.lighter_sky
+COLOR_LIGHTER_AZURE = libtcod.lighter_azure
+COLOR_LIGHTER_BLUE = libtcod.lighter_blue
+COLOR_LIGHTER_HAN = libtcod.lighter_han
+COLOR_LIGHTER_VIOLET = libtcod.lighter_violet
+COLOR_LIGHTER_PURPLE = libtcod.lighter_purple
+COLOR_LIGHTER_FUCHSIA = libtcod.lighter_fuchsia
+COLOR_LIGHTER_MAGENTA = libtcod.lighter_magenta
+COLOR_LIGHTER_PINK = libtcod.lighter_pink
+COLOR_LIGHTER_CRIMSON = libtcod.lighter_crimson
+
+COLOR_LIGHTEST_RED = libtcod.lightest_red
+COLOR_LIGHTEST_FLAME = libtcod.lightest_flame
+COLOR_LIGHTEST_ORANGE = libtcod.lightest_orange
+COLOR_LIGHTEST_AMBER = libtcod.lightest_amber
+COLOR_LIGHTEST_YELLOW = libtcod.lightest_yellow
+COLOR_LIGHTEST_LIME = libtcod.lightest_lime
+COLOR_LIGHTEST_CHARTREUSE = libtcod.lightest_chartreuse
+COLOR_LIGHTEST_GREEN = libtcod.lightest_green
+COLOR_LIGHTEST_SEA = libtcod.lightest_sea
+COLOR_LIGHTEST_TURQUOISE = libtcod.lightest_turquoise
+COLOR_LIGHTEST_CYAN = libtcod.lightest_cyan
+COLOR_LIGHTEST_SKY = libtcod.lightest_sky
+COLOR_LIGHTEST_AZURE = libtcod.lightest_azure
+COLOR_LIGHTEST_BLUE = libtcod.lightest_blue
+COLOR_LIGHTEST_HAN = libtcod.lightest_han
+COLOR_LIGHTEST_VIOLET = libtcod.lightest_violet
+COLOR_LIGHTEST_PURPLE = libtcod.lightest_purple
+COLOR_LIGHTEST_FUCHSIA = libtcod.lightest_fuchsia
+COLOR_LIGHTEST_MAGENTA = libtcod.lightest_magenta
+COLOR_LIGHTEST_PINK = libtcod.lightest_pink
+COLOR_LIGHTEST_CRIMSON = libtcod.lightest_crimson
+
+COLOR_DESATURATED_RED = libtcod.desaturated_red
+COLOR_DESATURATED_FLAME = libtcod.desaturated_flame
+COLOR_DESATURATED_ORANGE = libtcod.desaturated_orange
+COLOR_DESATURATED_AMBER = libtcod.desaturated_amber
+COLOR_DESATURATED_YELLOW = libtcod.desaturated_yellow
+COLOR_DESATURATED_LIME = libtcod.desaturated_lime
+COLOR_DESATURATED_CHARTREUSE = libtcod.desaturated_chartreuse
+COLOR_DESATURATED_GREEN = libtcod.desaturated_green
+COLOR_DESATURATED_SEA = libtcod.desaturated_sea
+COLOR_DESATURATED_TURQUOISE = libtcod.desaturated_turquoise
+COLOR_DESATURATED_CYAN = libtcod.desaturated_cyan
+COLOR_DESATURATED_SKY = libtcod.desaturated_sky
+COLOR_DESATURATED_AZURE = libtcod.desaturated_azure
+COLOR_DESATURATED_BLUE = libtcod.desaturated_blue
+COLOR_DESATURATED_HAN = libtcod.desaturated_han
+COLOR_DESATURATED_VIOLET = libtcod.desaturated_violet
+COLOR_DESATURATED_PURPLE = libtcod.desaturated_purple
+COLOR_DESATURATED_FUCHSIA = libtcod.desaturated_fuchsia
+COLOR_DESATURATED_MAGENTA = libtcod.desaturated_magenta
+COLOR_DESATURATED_PINK = libtcod.desaturated_pink
+COLOR_DESATURATED_CRIMSON = libtcod.desaturated_crimson
+
+COLOR_BRASS = libtcod.brass
+COLOR_COPPER = libtcod.copper
+COLOR_GOLD = libtcod.gold
+COLOR_SILVER = libtcod.silver
+
+COLOR_CELADON = libtcod.celadon
+COLOR_PEACH = libtcod.peach
